652ae4d505135dc3415611e3875888c4398ad580
luifean <luisfernandogvv@gmail.com>
2025-01-30 09:38:12 -0300

    feat: add prompt handling to central area controller

--- src/components/central-area/controller.ts
+++ src/components/central-area/controller.ts
@@ -1,9 +1,21 @@
+import { FormEvent, useState } from "react";
 import { useParams } from "react-router";
 
 export const useCentralAreaController = () => {
   const { id } = useParams();
 
+  const [promp, setPrompt] = useState("");
+
+  const handlePrompCommand = (event: React.ChangeEvent<HTMLTextAreaElement>) =>
+    setPrompt(event.target.value);
+
+  const handleOnFormSubmit = async (event: FormEvent<HTMLFormElement>) =>
+    event.preventDefault();
+
   return {
     id,
+    promp,
+    handlePrompCommand,
+    handleOnFormSubmit,
   };
 };
